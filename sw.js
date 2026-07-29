5dcfd6e62e1c3f4853a74b42923e162d6b82096e
Jorgefo1965 <jfonckechevarria@gmail.com>
2026-07-29 13:26:44 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'trancos-v2';
+const CACHE = 'trancos-v3';
 const FILES = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e=>{
